042cda558e996ad4f478075055d0cae7e7125126
jprodriguezg <juanpablo.rodriguezgomez@gmail.com>
2015-07-21 11:29:13 +0200

    improvements

--- set_waypoints/src/ar_drone_set_waypoints.cpp
+++ set_waypoints/src/ar_drone_set_waypoints.cpp
@@ -20,6 +20,19 @@ void hasReceivedModelState(const geometry_msgs::PoseStamped::ConstPtr& msg){
   return;
 }
 
+void new_quadrant(double quadrant[4][2], std::vector<double> central_point, double range){
+
+	quadrant[0][0] = central_point[0]-range/2;
+	quadrant[0][1] = central_point[1]+range/2;
+	quadrant[1][0] = central_point[0]+range/2;
+	quadrant[1][1] = central_point[1]+range/2;
+	quadrant[2][0] = central_point[0]+range/2;
+	quadrant[2][1] = central_point[1]-range/2;
+	quadrant[3][0] = central_point[0]-range/2;
+	quadrant[3][1] = central_point[1]-range/2;
+
+}
+
 int nearest_vertex(double quadrant[4][2]){
 	int index = 0;
 	double new_rho, rho = 1000;
@@ -38,55 +51,88 @@ int main(int argc, char** argv){
     
 ros::init(argc, argv, "set_waypoints_node");
 ros::NodeHandle nh_;
+ros::NodeHandle nhp_("~");
 ros::Rate rate(20.0);
 
 ros::Subscriber optitrack_sub_=nh_.subscribe("drone_info_topic", 10, hasReceivedModelState);
 ros::Publisher waypoint_info_pub_=nh_.advertise<drone_control_msgs::send_control_data>("waypoint_topic", 1);
 
 
-double rho, delta_time, range = 2.0;
-std::vector<double> central_point (2,0), final_pose (2,0);
+double rho, base_time = 0.0, delta_time = 0.0, range = 2.0;
+std::vector<double> central_point (2,0), final_pose (2,0), emergency_pose(2,0);
 drone_control_msgs::send_control_data waypoint_publish;
 
-nh_.getParam("/set_waypoints_node/range",range);
-nh_.getParam("/set_waypoints_node/central_point",central_point);
-nh_.getParam("/set_waypoints_node/final_position",final_pose);
-nh_.getParam("/set_waypoints_node/delta_time",delta_time);
+bool flag = true, emergency = false;
+int index = 0;
+double quadrant[4][2];
+// Original definition of quadrant
+/* double quadrant[4][2] = {{central_point[0]-range/2, central_point[1]+range/2},
+				{central_point[0]+range/2, central_point[1]+range/2},
+				{central_point[0]+range/2, central_point[1]-range/2},
+				{central_point[0]-range/2, central_point[1]-range/2}};*/
 
-double quadrant[4][2] = {{central_point[0]-range/2, central_point[1]+range/2},
-			{central_point[0]+range/2, central_point[1]+range/2},
-			{central_point[0]+range/2, central_point[1]-range/2},
-			{central_point[0]-range/2, central_point[1]-range/2}};
-
-int index = nearest_vertex(quadrant);
-	
 	while (ros::ok()){
 
-		if (delta_time > ros::Time::now().toSec()){
-			rho = sqrt(pow(quadrant[index][0]-Drone_info[0],2)+pow(quadrant[index][1]-Drone_info[1],2));
-			if (rho < 0.2){
-				if (index<3){
-					index++;
-				}
-				else{
-					index = 0;
+		nhp_.getParam("emergency_position",emergency_pose);
+		nhp_.getParam("emergency",emergency);
+
+		if(emergency == true){
+			waypoint_publish.position.x = emergency_pose[0];
+			waypoint_publish.position.y = emergency_pose[1];
+			flag = true;
+			nhp_.setParam("delta_time", 0.0);
+		}
+		
+		else{
+			
+
+			if (flag == true){
+				nhp_.getParam("range",range);
+				nhp_.getParam("central_point",central_point);
+				nhp_.getParam("final_position",final_pose);
+				nhp_.getParam("delta_time",delta_time);
+
+				if (delta_time != 0.0){
+
+					new_quadrant(quadrant,central_point,range);
+					index = nearest_vertex(quadrant);    
+					flag = false;
+					base_time = ros::Time::now().toSec();
 				}
 			}
-			waypoint_publish.position.x = quadrant[index][0];
-			waypoint_publish.position.y = quadrant[index][1];
-		}
-		else{
-			waypoint_publish.position.x = final_pose[0];
-			waypoint_publish.position.y = final_pose[1];
-		}
 
+			else{
+
+				if (base_time+delta_time > ros::Time::now().toSec()){
+					rho = sqrt(pow(quadrant[index][0]-Drone_info[0],2)+pow(quadrant[index][1]-Drone_info[1],2));
+					if (rho < 0.2){
+						if (index<3){
+							index++;
+						}
+						else{
+							index = 0;
+						}
+					}
+					waypoint_publish.position.x = quadrant[index][0];
+					waypoint_publish.position.y = quadrant[index][1];
+				}
+				else{
+					waypoint_publish.position.x = final_pose[0];
+					waypoint_publish.position.y = final_pose[1];
+					flag = true;
+					nhp_.setParam("delta_time", 0.0);
+					base_time = 0.0;
+				}
+			}
+		}
 	
 		waypoint_publish.position.z = 1.0;
 		waypoint_info_pub_.publish(waypoint_publish);
 
-		//std::cout <<central_point[0]<< " , " <<central_point[1]<<std::endl;
+		/*
+		std::cout <<delta_time<<std::endl;
 		std::cout <<rho<<std::endl;
-		std::cout <<ros::Time::now()<<std::endl;
+		std::cout <<ros::Time::now()<<std::endl;*/
 		
 
 		
